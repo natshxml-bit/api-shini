397043e00a37ff9913abac8e6445ebb5666586d8
natshxml-bit <natshxml@gmail.com>
2026-06-14 15:24:45 +0700

    fixx home

--- src/home.js
+++ src/home.js
@@ -8,8 +8,19 @@ const TARGET_URL = 'https://web1.mgkomik.cc/';
 
 router.get('/', async (req, res) => {
     try {
-        // Fetch HTML dari web target
-        const { data } = await axios.get(TARGET_URL);
+        // Fetch HTML dari web target dengan Header Palsu biar ga kena 403
+        const { data } = await axios.get(TARGET_URL, {
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                'Accept-Language': 'id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7',
+                'Referer': 'https://www.google.com/',
+                'sec-ch-ua': '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"Windows"'
+            }
+        });
+        
         const $ = cheerio.load(data);
 
         const projectUpdate = [];
@@ -98,4 +109,3 @@ router.get('/', async (req, res) => {
 });
 
 module.exports = router;
-
